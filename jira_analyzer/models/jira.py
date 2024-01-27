f90ccf95c396263f085e9ae8f1e1965fb70e3aa9
Mihalis <mihalispapak@gmail.com>
2024-01-27 17:10:25 +0200

    add task orm entity

--- jira_analyzer/models/jira.py
+++ jira_analyzer/models/jira.py
@@ -1,6 +1,20 @@
+from typing import List
+
+from sqlalchemy import orm, ForeignKey
 from jira_analyzer.shared import model
 
 
 class Project(model.ModelBase):
     __tablename__ = "project"
     __id_prefix__ = "prj-"
+
+    tasks: orm.Mapped[List["Task"]] = orm.relationship(back_populates="project")
+
+
+class Task(model.ModelBase):
+    __tablename__ = "task"
+    __id_prefix__ = "tsk-"
+
+    project_id: orm.Mapped[str] = orm.mapped_column(ForeignKey("project.id"))
+    project: orm.Mapped["Project"] = orm.relationship(back_populates="tasks")
+
