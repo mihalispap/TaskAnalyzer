d8e7bc039fcb3953d8f597cc952d220396b322e5
Mihalis <mihalispapak@gmail.com>
2024-01-27 17:02:39 +0200

    add project ingestion test script

--- jira_analyzer/main.py
+++ jira_analyzer/main.py
@@ -1,4 +1,10 @@
 from jira_analyzer.clients import jira
+from jira_analyzer.services import actions
 
 jira_client = jira.JiraClient()
-jira_client.get_projects()
+for project in jira_client.get_projects():
+    actions.create_or_update_project(
+        external_id=project.get('id'),
+        datasource='JIRA',
+        name=project.get('name'),
+    )
