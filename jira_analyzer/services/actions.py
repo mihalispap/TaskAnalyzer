4e9d7e0dbc5db3e9ba79306e5544374ef1988152
Mihalis <mihalispapak@gmail.com>
2024-01-28 13:12:17 +0200

    add creation/update method for assignees

--- jira_analyzer/services/actions.py
+++ jira_analyzer/services/actions.py
@@ -35,6 +35,35 @@ def create_or_update_project(
     return entity
 
 
+def create_or_update_assignee(
+        external_id: str,
+        datasource: str,
+        name: str,
+) -> jira.Project:
+    is_new = False
+    with db.session_scope() as session:
+        repo = repos.AssigneeRepo(session)
+        entity = repo.get_by_external_id_and_datasource(
+            external_id=external_id,
+            datasource=datasource,
+        )
+        if not entity:
+            entity = jira.Assignee(
+                name=name,
+                external_id=external_id,
+                datasource=datasource,
+            )
+            is_new = True
+        entity.name = name
+        entity.external_id = external_id
+        entity.datasource = datasource
+
+        if is_new:
+            entity = repo.save(entity)
+
+    return entity
+
+
 def create_or_update_task(
         external_id: str,
         datasource: str,
