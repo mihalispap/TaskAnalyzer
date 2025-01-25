bda26d0d6c593f61e62bf8c9468d95122d7131c7
Mihalis <mihalispapak@gmail.com>
2025-01-25 16:29:42 +0200

    add statusrepo

--- task_analyzer/repos/repos.py
+++ task_analyzer/repos/repos.py
@@ -7,10 +7,16 @@ from task_analyzer.models import task_analyzer_models
 from task_analyzer.shared import repos
 
 
+class StatusRepo(repos.BaseRepo[task_analyzer_models.Status]):
+    """Query repository for Statuses."""
+    _type = task_analyzer_models.Status
+
+
 class ProjectRepo(repos.BaseRepo[task_analyzer_models.Project]):
     """Query repository for Projects."""
     _type = task_analyzer_models.Project
 
+    # TODO: remove
     def get_by_external_id_and_datasource(
             self,
             external_id: str,
@@ -30,6 +36,7 @@ class TaskRepo(repos.BaseRepo[task_analyzer_models.Task]):
     """Query repository for Tasks."""
     _type = task_analyzer_models.Task
 
+    # TODO: remove
     def get_by_external_id_and_datasource(
             self,
             external_id: str,
@@ -49,6 +56,7 @@ class UserRepo(repos.BaseRepo[task_analyzer_models.User]):
     """Query repository for Users."""
     _type = task_analyzer_models.User
 
+    # TODO: remove
     def get_by_external_id_and_datasource(
             self,
             external_id: str,
